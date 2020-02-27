adf7822507d692d3a02f4b528edd271ab7447ba1
FenomenCoyote <javcruz@ucm.es>
2020-02-27 12:44:05 +0100

    Load State
    Load State comentado con codigo que serviria para Screen Loader State

--- AnimalCooking/LoadState.h
+++ AnimalCooking/LoadState.h
@@ -1,7 +1,12 @@
 #pragma once
 #include "State.h"
-class LoadState :
-	public State
+class LoadState : public State
 {
+public:
+	LoadState(/*Resources::Nivel nivel*/); // Resources::Nivel nivel: es un enum que indica que recursos cargar en memoria
+//	void resetResources(Resources::Nivel nivel); // Resources::Nivel nivel: es un enum que indica que recursos cargar en memoria
+//private:
+//	Entity* barraCarga_;
+//	Entity* buttonGo_;
 };
 
