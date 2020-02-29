9eb34f158284eae6670c1075cbea0644d29f3503
nicoFdez <47755232+nicoFdez@users.noreply.github.com>
2020-02-29 21:21:24 +0100

    Update de ButtonBehaviour casi implementado
    Falta el acceso a la instancia de AnimalCooking para acceder a la FSM

--- AnimalCooking/ButtonBehaviour.cpp
+++ AnimalCooking/ButtonBehaviour.cpp
@@ -14,5 +14,14 @@ void ButtonBehaviour::init()
 
 void ButtonBehaviour::update()
 {
+	InputHandler* ih = InputHandler::instance();
+	Vector2D buttonPos = ownerTransform_->getPos();
+	Vector2D mousePos = ih->getMousePos();
+	
+	SDL_Point mousePosition = { mousePos.getX(), mousePos.getY() };
+	SDL_Rect buttonRect = RECT(buttonPos.getX(), buttonPos.getY(), ownerTransform_->getW(), ownerTransform_->getH());
 
+	if (SDL_PointInRect( &mousePosition, &buttonRect) && ih->getMouseButtonState(InputHandler::MOUSEBUTTON::LEFT)) {
+		//callback_(game_); Como la FSM esta en AnimalCooking necesito acceso a la instancia de animalcooking 
+	}
 }
