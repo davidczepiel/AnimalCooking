94c126bcb3cc5660ab97fda2b28a18cc27dc371c
PabloVillapunMartin <pvillapu@ucm.es>
2020-03-02 18:27:39 +0100

    MenuState class
    playMenuCallback y optionsMenuCallback aun sin implementar
    Falta centrar los botones en la pantalla

--- AnimalCooking/MenuState.cpp
+++ AnimalCooking/MenuState.cpp
@@ -1,1 +1,15 @@
 #include "MenuState.h"
+
+MenuState::MenuState() : State() {
+	cout << "Menu State" << endl;
+	playMenuButton = stage->addEntity();
+	optionsMenu = stage->addEntity();
+
+	playMenuButton->addComponent<Transform>()->setPos(Vector2D(10, 10));	//cambiar valores
+	playMenuButton->addComponent<ButtonRenderer>();
+	playMenuButton->addComponent<ButtonBehaviour>();
+
+	optionsMenu->addComponent<Transform>()->setPos(Vector2D(10, 40));		//cambiar valores
+	optionsMenu->addComponent<ButtonRenderer>();
+	optionsMenu->addComponent<ButtonBehaviour>();
+}
--- AnimalCooking/MenuState.h
+++ AnimalCooking/MenuState.h
@@ -1,8 +1,21 @@
 #pragma once
 #include "State.h"
+#include "ButtonBehaviour.h"
+#include "ButtonRenderer.h"
+#include "Transform.h"
+
 class MenuState: public State
 {
+	Entity* playMenuButton = nullptr;
+	Entity* optionsMenu = nullptr;
+
+	Transform* playMenuTr_ = nullptr;
+	Transform* optionsMenuTr_ = nullptr;
+
+	static void playMenuCallback();		//Hacer callback cuando se incluya *game en State
+	static void optionsMenuCallback();	//Hacer callback cuando se incluya *game en State
 public:
-	MenuState() :State() { }
+	MenuState();
+	~MenuState();
 };
 
