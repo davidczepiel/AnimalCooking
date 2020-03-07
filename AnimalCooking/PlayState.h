008ecf1956004aaa5ba0196995d7b52b52908002
Javier Cruz López de Ochoa <javcruz@ucm.es>
2020-03-07 11:10:35 +0100

    Update playState

--- AnimalCooking/PlayState.h
+++ AnimalCooking/PlayState.h
@@ -7,20 +7,7 @@
 class PlayState : public State
 {
 public:
-	PlayState() : State() {
-		Entity* morir = stage->addEntity();
-		morir->addComponent<Transform>(Vector2D(SDLGame::instance()->getWindowWidth() / 2, 50),Vector2D(),300,100,0);
-		morir->addComponent<ButtonBehaviour>(goToEndState);
-		morir->addComponent<ButtonRenderer>(SDLGame::instance()->getTextureMngr()->getTexture(Resources::Button),nullptr);
-		Entity* pausar = stage->addEntity();
-		pausar->addComponent<Transform>(Vector2D(SDLGame::instance()->getWindowWidth() / 2, 150), Vector2D(), 300, 100, 0);
-		pausar->addComponent<ButtonBehaviour>(goToPauseState);
-		pausar->addComponent<ButtonRenderer>(SDLGame::instance()->getTextureMngr()->getTexture(Resources::Button), nullptr);
-	}
-	void update() override { 
-		State::update(); 
-		cout << "Me gustan las patatas" << endl; };
-	//void draw() override {};
+	PlayState() : State() {}
 
 	//virtual void handleEvent();
 private:
