c9bbc1bba7e3419cb0b9deca66726ebbd5bb8087
PabloVillapunMartin <pvillapu@ucm.es>
2020-03-09 18:58:06 +0100

    preparar para merge

--- AnimalCooking/PlayState.h
+++ AnimalCooking/PlayState.h
@@ -1,37 +1,17 @@
 #pragma once
 
 #include "State.h"
-#include "IngredientsPool.h"
-#include "IngredientViewer.h"
-#include "Ingredient.h"
-#include "IngredientMotion.h"
-
+#include "EndState.h"
+#include "PauseState.h"
+#include "Manager.h"
 class PlayState : public State
 {
 public:
-	PlayState() : State() {
-		cout << "PlayState" << endl;
-		Entity* e = stage->addEntity();
-		e->addComponent<IngredientsPool>();
-		e->addComponent<IngredientViewer>();
-		e->addComponent<IngredientMotion>();
-		Tomato* t = new Tomato();
-		
-		t->setTransform(50, 50, Vector2D(100, 100), Vector2D(1, 1));
-		e->getComponent<IngredientsPool>(ecs::IngredientsPool)->addIngredient(t);
-		t = new Tomato();
-		t->setTransform(50, 50, Vector2D(400, 100), Vector2D(-1, 1));
-		e->getComponent<IngredientsPool>(ecs::IngredientsPool)->addIngredient(t);
-		t->destroy(0);
-	}
-	void update() override {
-		stage->update();
-	};
-	void draw() override {
-		stage->draw();
-	};
+	PlayState() : State() {}
 
 	//virtual void handleEvent();
 private:
-
+	static void goToEndState();
+	static void goToPauseState();
 };
+
