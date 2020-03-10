582c38eae6080d26613d067ae848e378f8d3de5d
nicoFdez <47755232+nicoFdez@users.noreply.github.com>
2020-03-10 11:23:26 +0100

    PlayState devuelto a la normalidad

--- AnimalCooking/PlayState.h
+++ AnimalCooking/PlayState.h
@@ -8,16 +8,7 @@
 class PlayState : public State
 {
 public:
-	PlayState() : State() {
-		Entity* foodMngr = stage->addEntity();
-		foodMngr->addComponent<FoodPool>();
-		foodMngr->addComponent<FoodViewer>();
-		foodMngr->addComponent<FoodMotion>();
-
-		SlicedTomato* st = new SlicedTomato(Vector2D(100, 100));
-		foodMngr->getComponent<FoodPool>(ecs::FoodPool)->AddFood(st);
-
-	}
+	PlayState() : State() {}
 	void update() override { cout << "Me gustan las patatas" << endl; };
 	void draw() override {};
 
