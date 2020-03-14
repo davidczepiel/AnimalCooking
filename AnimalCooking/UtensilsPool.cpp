6735c54b7d2c786a730ef69a53a56a3794adbc8f
czepieldavid <dczepiel@ucm.es>
2020-03-14 18:32:42 +0100

    Solucionados problemas ejecucion UtensilsPool

--- AnimalCooking/UtensilsPool.cpp
+++ AnimalCooking/UtensilsPool.cpp
@@ -5,13 +5,13 @@ UtensilsPool::UtensilsPool() : Component(ecs::UtensilsPool)
 	//Se podría meter en un orden concreto para que al preparar los niveles siempre estén ordenados en las repisas
 	//porque al empezar se hace disableall y al hacer getObj te los da en ese orden concreto
 		Knife* t = new Knife(Vector2D(1,1),nullptr, nullptr);
-		pool_.at(0) = t;
+		pool_.push_back(t);
 		Mace* m = new Mace(Vector2D(1, 1), nullptr, nullptr);
-		pool_.at(1) = m;
+		pool_.push_back(m);
 		Grater* g = new Grater(Vector2D(1, 1), nullptr, nullptr);
-		pool_.at(2) = g;
+		pool_.push_back(g);
 		Net* n = new Net(Vector2D(1, 1), nullptr, nullptr);
-		pool_.at(3) = n;
+		pool_.push_back(n);
 }
 
 
