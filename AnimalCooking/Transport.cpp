b3b7ca6e0c46a98792e6e97c9503d63ff5b15060
nicoFdez <47755232+nicoFdez@users.noreply.github.com>
2020-03-16 20:47:45 +0100

    Movimiento del transport apañao

--- AnimalCooking/Transport.cpp
+++ AnimalCooking/Transport.cpp
@@ -34,12 +34,50 @@ void Transport::update()
 {	
 	if (objInHands_ != nullptr && !(playerTransform_->getVel().getX() == 0 && playerTransform_->getVel().getY() == 0)) {
 
-		int angle = atan2(playerTransform_->getVel().getY(), playerTransform_->getVel().getX());
-
+		float angle = -((atan2(playerTransform_->getVel().getY(), playerTransform_->getVel().getX()) * 180)/M_PI);
 		int centerX = playerTransform_->getPos().getX() + playerTransform_->getW() / 2;
 		int centerY = playerTransform_->getPos().getY() + playerTransform_->getH() / 2;
 
-		objInHands_->setPos(Vector2D(centerX, centerY) + Vector2D(0, -(playerTransform_->getH()/2)).rotate((angle*180)/M_PI));
+		Vector2D objPos = Vector2D();
+		double offsetX = playerTransform_->getW()/2;
+		double offsetY = playerTransform_->getH()/2;
+
+		Vector2D objOffset = objInHands_->getSize()/2;
+
+		if ((angle > (-22.5) && angle <= 22.5)) { //Derecha
+			cout << "Derecha" << endl;
+			objPos = Vector2D(centerX + offsetX, centerY);
+		}
+		else if((angle > 22.5 && angle <= 67.5)) { //Arriba a la derecha
+			cout << "Arriba a la derecha" << endl;
+			objPos = Vector2D(centerX + offsetX, centerY - offsetY);
+		}
+		else if ((angle > 67.5 && angle <= 112.5)) { //Arriba
+			cout << "Arriba" << endl;
+			objPos = Vector2D(centerX, centerY - offsetY);
+		}
+		else if ((angle > 112.5 && angle <= 157.5)) { //Arriba a la izquierda
+			cout << "Arriba a la izquierda" << endl;
+			objPos = Vector2D(centerX - offsetX, centerY - offsetY);
+		}
+		else if ((angle > 157.5 || angle <= (-157.5))) { //izquierda
+			cout << "izquierda" << endl;
+			objPos = Vector2D(centerX - offsetX, centerY);
+		}
+		else if ((angle > (-157.5) && angle <= (-112.5))) { //Abajo a la izquierda
+			cout << "Abajo a la izquierda" << endl;
+			objPos = Vector2D(centerX - offsetX, centerY + offsetY);
+		}
+		else if ((angle > (-112.5) && angle <= (-67.5))) { //Abajo
+			cout << "Abajo" << endl;
+			objPos = Vector2D(centerX, centerY + offsetY);
+		}
+		else if ((angle > (-67.5) && angle <= (-22.5))) { //abajo a la derecha
+			cout << "abajo a la derecha" << endl;
+			objPos = Vector2D(centerX + offsetX, centerY + offsetY);
+		}
+
+		objInHands_->setPos(objPos - objOffset);
 	}
 
 	/*if (objInHands_ != nullptr && !(playerTransform_->getVel().getX() == 0 && playerTransform_->getVel().getY() == 0)) {
