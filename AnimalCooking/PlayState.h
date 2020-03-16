a8460f51f3f47e0f8afdc4271c949e7cafe48d66
nicoFdez <47755232+nicoFdez@users.noreply.github.com>
2020-03-16 13:36:53 +0100

    Transport funciona pero el movimiento va un poco mal
    Añadida la destructora de la FoodPool

--- AnimalCooking/PlayState.h
+++ AnimalCooking/PlayState.h
@@ -9,17 +9,35 @@
 #include "PlayerViewer.h"
 #include "Entity.h"
 
+#include "FoodPool.h"
+#include "FoodMotion.h"
+#include "FoodViewer.h"
+#include "Transport.h"
+
+#include "Food.h"
+
 class PlayState : public State
 {
 public:
 	PlayState() : State() {
 		
 		Entity* player = stage->addEntity();
-		player->addComponent<Transform>();
+		player->addComponent<Transform>(Vector2D(), Vector2D(), 64, 128, 0);
 		player->addComponent<PlayerMotion>();
 		player->addComponent<PlayerController>();
 		player->addComponent<PlayerViewer>(SDLGame::instance()->getTextureMngr()->getTexture(Resources::Cerdo));
 		
+		Transport* transportP1 = player->addComponent<Transport>();
+
+		Entity* foodMngr = stage->addEntity();
+		FoodPool* fp = foodMngr->addComponent<FoodPool>();
+		foodMngr->addComponent<FoodViewer>();
+		foodMngr->addComponent<FoodMotion>();
+
+		SlicedTomato* st = new SlicedTomato(Vector2D(50, 50), transportP1, nullptr);
+		fp->AddFood(st);
+
+		transportP1->pick(st);
 	}
 
 
--- AnimalCooking/Transport.cpp
+++ AnimalCooking/Transport.cpp
@@ -5,7 +5,6 @@ Transport::Transport() : Component(ecs::Transport)
 {
 	objInHands_ = nullptr;
 	playerTransform_ = nullptr;
-	emptyDish_ = false;
 }
 
 void Transport::pick(Pickable* obj)
@@ -32,14 +31,29 @@ void Transport::init()
 }
 
 void Transport::update()
-{
-	if (objInHands_ != nullptr) { objInHands_->setSpeed(playerTransform_->getVel()); }
+{	
+	if (objInHands_ != nullptr && !(playerTransform_->getVel().getX() == 0 && playerTransform_->getVel().getY() == 0)) {
+
+		int angle = atan2(playerTransform_->getVel().getY(), playerTransform_->getVel().getX());
+
+		int centerX = playerTransform_->getPos().getX() + playerTransform_->getW() / 2;
+		int centerY = playerTransform_->getPos().getY() + playerTransform_->getH() / 2;
+
+		objInHands_->setPos(Vector2D(centerX, centerY) + Vector2D(0, -(playerTransform_->getH()/2)).rotate((angle*180)/M_PI));
+	}
+
+	/*if (objInHands_ != nullptr && !(playerTransform_->getVel().getX() == 0 && playerTransform_->getVel().getY() == 0)) {
+		
+		objInHands_->setPos(Vector2D(playerTransform_->getPos().getX() + playerTransform_->getW()/2 + playerTransform_->getVel().getX() * playerTransform_->getW()/2,
+			playerTransform_->getPos().getY() + playerTransform_->getH()/2 + playerTransform_->getVel().getY() * playerTransform_->getH()/2));
+	}*/
 }
 
 bool Transport::hasEmptyDish()
 {
-	if (dynamic_cast<Dish*>(objInHands_) != nullptr) {
+	/*if (dynamic_cast<Dish*>(objInHands_) != nullptr) {
 		return static_cast<Dish*>(objInHands_)->isEmpty();
 	}
-	else return false;
+	else return false;*/
+	return false;
 }
