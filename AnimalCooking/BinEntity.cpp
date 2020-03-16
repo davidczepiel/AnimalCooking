07084a4ab5db1ff414b23ec8e6f5b81fafa59a94
PabloVillapunMartin <pvillapu@ucm.es>
2020-03-16 13:27:25 +0100

    logica para limpiar platos

--- AnimalCooking/BinEntity.cpp
+++ AnimalCooking/BinEntity.cpp
@@ -2,10 +2,17 @@
 
 void BinEntity::interactive(int player)
 {
+	Dish* d;
 	if (Resources::Player1 == player) {
-
+		d = dynamic_cast<Dish*>(player1_->getObjectInHands());
+		if (d != nullptr) {
+			d->clearDish(); //Limpiar el plato
+		}
 	}
 	else {
-
+		d = dynamic_cast<Dish*>(player2_->getObjectInHands());
+		if (d != nullptr) {
+			d->clearDish(); //Limpiar el plato
+		}
 	}
 }
