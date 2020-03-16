8be8dbd7047b2b5398189c8a97f1eb6c5de77256
MarcoIMH <47715985+MarcoIMH@users.noreply.github.com>
2020-03-16 18:19:34 +0100

    IMPLEMENTACIÓN DISHES
    Actualización manual para sortear el fallo de intento de fusión.

--- AnimalCooking/DishPool.h
+++ AnimalCooking/DishPool.h
@@ -1,1 +1,20 @@
 #pragma once
+#include "Component.h"
+#include <vector>
+#include"Dish.h"
+
+class DishPool : public Component
+{
+public:
+	DishPool();
+    ~DishPool() { }
+
+	//Metodo auxiliar para meter platos llamar al de DishStack
+	void addDish(Dish* d);
+	void removeDish(Dish* d);
+	int getNumDishes() { return dishes_.size(); }
+	inline vector<Dish*>& getDishes() { return dishes_; }
+
+private:
+	vector<Dish*> dishes_;
+};
---
+++ AnimalCooking/DishStack.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Component.h"
+#include "DishPool.h"
+
+class DishStack : public Component
+{
+public:
+
+	DishStack(int maxDishes_);
+	 ~DishStack() { }
+
+	void init() override;
+	Dish* addNewDish(Vector2D pos);
+	void removeDish(Dish* d);
+
+private:
+	int maxDishes;
+	DishPool* dishPool = nullptr;
+};
+
