6c64cfe638355211d157fc12efd000402fd4ecc9
MarcoIMH <47715985+MarcoIMH@users.noreply.github.com>
2020-03-23 14:14:18 +0100

    UPDATED: FoodConfig  -> Knife recipes
    Añadidos los recipes del cuchillo

--- AnimalCooking/FoodConfig.cpp
+++ AnimalCooking/FoodConfig.cpp
@@ -7,4 +7,34 @@ FoodConfig::FoodConfig()
 
 void FoodConfig::fill()
 {
+	///////////////////////////////////////////////////////////////////////////////////
+	//Fill transformations1:  Ingredient -> Normal food into foodKilled recipes.//////
+	/////////////////////////////////////////////////////////////////////////////////
+	vector <utensilsTrans> uTrans;
+
+	///////////////////////
+	// KNIFE  RECIPES    // -------> SET <--------------   -----------> RESULT <--------------
+	//////////////////////	
+	uTrans.push_back({ Resources::IngredientType::carrot,	Resources::FoodType::SlicedCarrot });
+	uTrans.push_back({ Resources::IngredientType::cheese,	Resources::FoodType::SlicedCheese });
+	uTrans.push_back({ Resources::IngredientType::chicken,  Resources::FoodType::SlicedChicken });
+	uTrans.push_back({ Resources::IngredientType::lettuce,  Resources::FoodType::SlicedLettuce });
+	uTrans.push_back({ Resources::IngredientType::meat,		Resources::FoodType::SlicedMeat });
+	uTrans.push_back({ Resources::IngredientType::mushroom, Resources::FoodType::SlicedMushroom });
+	uTrans.push_back({ Resources::IngredientType::onion,	Resources::FoodType::SlicedOnion });
+	uTrans.push_back({ Resources::IngredientType::potato,	Resources::FoodType::SlicedPotato });
+	//uTrans.push_back({ Resources::IngredientType::potato,	Resources::FoodType::SlicedPotatoSkillet });
+	uTrans.push_back({ Resources::IngredientType::sausage,	Resources::FoodType::SlicedSausage });
+	uTrans.push_back({ Resources::IngredientType::tomato,	Resources::FoodType::SlicedTomato });
+		
+
+	//Adding Knife recipes
+	utensilsRecipes_.push_back({ Resources::UtensilType::Knife, uTrans });
+	
+
+
+
+
+
+	
 }
