c48047bfd89788c5647932a3e5e41e223e436eff
nicoFdez <47755232+nicoFdez@users.noreply.github.com>
2020-04-07 19:17:02 +0200

    Todo Sweet diccionario implementado

--- AnimalCooking/GameLogic.cpp
+++ AnimalCooking/GameLogic.cpp
@@ -11,10 +11,6 @@ void GameLogic::hitIngredient(SDL_Rect rect, Resources::UtensilType type)
     for (Ingredient* ing : ingPool->getPool()) { //colision temporal
 
         if (Collisions::collides(Vector2D(rect.x, rect.y), rect.w, rect.h, ing->getPos(), ing->getWidth(), ing->getHeight())) {
-            
-            /*Vector2D ingPos = ing->getPos();
-            Resources::FoodType result = ing->destroy(type);
-            GETCMP1_(GameControl)->newFood(result, ingPos);*/
 
             Vector2D ingPos = ing->getPos();
             Resources::IngredientType ingType = ing->getType();
@@ -23,6 +19,7 @@ void GameLogic::hitIngredient(SDL_Rect rect, Resources::UtensilType type)
             GETCMP1_(GameControl)->newFood(f, ingPos);
 
             //GETCMP1_(GameControl)->newIngredient();
+
             break;
         }
     }
