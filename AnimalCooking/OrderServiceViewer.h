5569b1428a56899b7afa85fdb03987140d6bc469
czepieldavid <dczepiel@ucm.es>
2020-04-17 16:42:30 +0100

    Reajustes en OrderService

--- AnimalCooking/OrderServiceViewer.h
+++ AnimalCooking/OrderServiceViewer.h
@@ -2,10 +2,7 @@
 #include "Component.h"
 #include "Vector2D.h"
 #include "Entity.h"
-#include "Transform.h"
 #include "SDL_macros.h"
-#include "Shelf.h"
-#include "Interactive.h"
 class OrderServiceViewer :
 	public Component
 {
