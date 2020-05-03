9eb59d8e5b1c28cc1948e842d1aacae573749977
davidczepiel <48888283+davidczepiel@users.noreply.github.com>
2020-05-03 21:22:43 +0200

    Adversidad del gancho funcional

--- AnimalCooking/AdversityManager.cpp
+++ AnimalCooking/AdversityManager.cpp
@@ -20,19 +20,20 @@ void AdversityManager::stopAdversity()
 {
 	playingAdversity = false;
 	myAdversity->reset();
+	
 }
 
 void AdversityManager::loadAdversity(ecs::AdversityID type)
 {
 	switch (type) {
 		case ecs::AdversityID::CookersAdversity:
-			myAdversity = new BurnedCookerAdversity(this);
+			//myAdversity = new BurnedCookerAdversity(this);
 		break;
 		case ecs::AdversityID::HookAdversity:
-			myAdversity = new HookAdversity(this);
+			myAdversity = new HookAdversity(this,nullptr);
 		break;
 		case ecs::AdversityID::PlaneAdversity:
-			myAdversity = new PlaneAdversity(this);
+			myAdversity = new PlaneAdversity(this,nullptr);
 		break;
 		case ecs::AdversityID::RainAdversity:
 			//myAdversity = new RainAdversity(this);
