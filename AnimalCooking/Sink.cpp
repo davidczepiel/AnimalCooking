92c8f2c67a195bc595131d32040dcebc62a926f4
davidczepiel <48888283+davidczepiel@users.noreply.github.com>
2020-05-08 20:29:27 +0200

    Fregadero con "Animacion" y reajustados tamaños de la cocina y los ingredienets

--- AnimalCooking/Sink.cpp
+++ AnimalCooking/Sink.cpp
@@ -2,7 +2,7 @@
 #include "GameConfig.h"
 
 Sink::Sink(Vector2D pos,Transport* p1, Transport* p2, EntityManager* mng) :Entity(SDLGame::instance(),mng), Interactive(p1, p2,nullptr), nTries(), lastTry() {
-	addComponent<SinkViewer>(this);
+	sV =addComponent<SinkViewer>(this);
 	position_ = pos;
 	size_ = Vector2D(128, 128);
 	maxTries = SDLGame::instance()->getRandGen()->nextInt(config::SINK_MIN_TRIES, config::SINK_MAX_TRIES);
@@ -14,6 +14,7 @@ Sink::~Sink() {
 void Sink::action1(int iDp) {
 	if (SDLGame::instance()->getTime() - lastTry < config::SINK_CADENCE) {
 		++nTries;
+		sV->setOnAction(true);
 		lastTry = SDLGame::instance()->getTime();
 		//Dependiendo del numero que me llegue me trabajo con el player 1 o 2
 		Transport* player;
@@ -24,11 +25,13 @@ void Sink::action1(int iDp) {
 		if (nTries >= maxTries && player->getObjectTypeInHands() == Resources::PickableType::Utensil) {
 			static_cast<Utensil*>(player->getObjectInHands())->cleanUp();
 			nTries = 0;
+			sV->setOnAction(false);
 			maxTries = SDLGame::instance()->getRandGen()->nextInt(config::SINK_MIN_TRIES, config::SINK_MAX_TRIES);
 		}	
 	}
 	else {
 		nTries = 0;
 		lastTry = SDLGame::instance()->getTime();
+		sV->setOnAction(false);
 	}
 }
