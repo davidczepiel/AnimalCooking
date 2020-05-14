eca7825a3bad6f527de4690d7f91b7bdb6c2772f
MarcoIMH <47715985+MarcoIMH@users.noreply.github.com>
2020-05-14 12:10:34 +0200

    re

--- AnimalCooking/MapState.h
+++ AnimalCooking/MapState.h
@@ -6,9 +6,10 @@ struct levelInfo {
 	string name = "";
 	unsigned int stars = 0;
 	string lore = "";
-	SDL_Rect house;
+	SDL_Rect houseBox = SDL_Rect();
 	Vector2D housePosition = Vector2D();
 	Vector2D buttonPosition = Vector2D();
+	bool unlocked = false;
 };
 
 class MapState :public State{
