9a80eb2990347af531af8a93fc89eec17a817368
PabloVillapunMartin <pvillapu@ucm.es>
2020-05-16 20:44:51 +0200

    Bug coger plato Transport

--- AnimalCooking/Transport.h
+++ AnimalCooking/Transport.h
@@ -2,10 +2,11 @@
 #include "Component.h"
 #include "Transform.h"
 #include "Pickable.h"
+#include "InteractionRect.h"
 
 class Transport : public Component {
 public:
-	Transport();
+	Transport(InteractionRect* ir);
 
 	void pick(Pickable* obj, Resources::PickableType objType, bool inFloor = true);
 	void drop(bool onFloor = true);
@@ -22,6 +23,7 @@ public:
 private:
 	Pickable* objInHands_;
 	Transform* playerTransform_;
+	InteractionRect* interactionPlayerRect;
 	Resources::PickableType objType_;
 	Vector2D dir;
 	void swap(Pickable* obj, Resources::PickableType objType, bool inFloor = true);
