cd3fcc9d030a6bb972d92c4e28e65f848f338056
Alonefcp <fcuadra@ucm.es>
2020-05-26 17:08:12 +0200

    Limpieza

--- AnimalCooking/PlayState.cpp
+++ AnimalCooking/PlayState.cpp
@@ -3,16 +3,6 @@
 #include "PauseState.h"
 #include "EndState.h"
 #include "Timer.h"
-/*void PlayState::handleEvent()
-{
-	InputHandler* ih = InputHandler::instance();
-	ih->update();
-	if (ih->keyDownEvent()) {
-		if (ih->isKeyDown(SDLK_p)) {
-			//game_->getFSM()->pushState(new PauseState());
-		}
-	}
-}*/
 
 void PlayState::pauseTimers()
 {
