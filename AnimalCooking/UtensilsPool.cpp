28eb1768b2d0cd75e3da75e3fefa94d91a7ff27c
davidczepiel <48888283+davidczepiel@users.noreply.github.com>
2020-08-06 12:07:23 +0200

    PopUpAparecen con el L1

--- AnimalCooking/UtensilsPool.cpp
+++ AnimalCooking/UtensilsPool.cpp
@@ -1,6 +1,6 @@
 #include "UtensilsPool.h"
 
-UtensilsPool::UtensilsPool(size_t size) : Component(ecs::UtensilsPool)
+UtensilsPool::UtensilsPool(size_t size) : displayIcons_(false),iconDisplayStart_(0),iconDisplayTime_(2000), Component(ecs::UtensilsPool)
 {
     pool_.reserve(size);
 }
@@ -19,6 +19,31 @@ void UtensilsPool::SetGameLogic(GameLogic* gl)
 		}
 	}
 
+}
+
+void UtensilsPool::update()
+{
+	if (!displayIcons_) {
+		if (GPadController::instance()->playerPressed(0, SDL_CONTROLLER_BUTTON_LEFTSHOULDER) || GPadController::instance()->playerPressed(1, SDL_CONTROLLER_BUTTON_LEFTSHOULDER)) {
+			iconDisplayStart_ = SDL_GetTicks();
+			changeDisplay(true);
+		}
+	}
+	else {
+		if (SDL_GetTicks() - iconDisplayStart_ > iconDisplayTime_) {
+			changeDisplay(false);
+		}
+	}
+}
+
+void UtensilsPool::changeDisplay(bool b)
+{
+	displayIcons_ = b;
+	for (int i = 0; i < pool_.size(); i++) {
+		if (pool_.at(i) != nullptr) {
+			pool_.at(i)->setDisplayIcon(b);
+		}
+	}
 }
 
 
