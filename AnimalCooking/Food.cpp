ed75b3e5f42a575a7ffd0245334f47512bf5cde8
Antonio Jesús Guerra Garduño <antgue01@ucm.es>
2020-08-12 11:21:04 +0200

    rect

--- AnimalCooking/Food.cpp
+++ AnimalCooking/Food.cpp
@@ -80,9 +80,9 @@ void Food::draw()
 			lastFrameTime_ = time;
 
 		}
-		destRect.w += .2 * destRect.w;
-		destRect.x -= .05 * destRect.w;
-		destRect.h += .1 * destRect.h;
+		destRect.w += .4 * destRect.w;
+		destRect.x -= .15 * destRect.w;
+		destRect.h += .2 * destRect.h;
 		explosion_->renderFrame(destRect, 0, explosionFrame_, 0);
 			/*if (explosionFrame_ == explosion_->getNumCols())
 			{
