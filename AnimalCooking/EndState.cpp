885f30d6da083b3c3e8ee20e54d2705fd1ec5549
Antonio Jesús Guerra Garduño <antgue01@ucm.es>
2020-09-17 13:12:23 +0200

    Bug de que no guarda en el EndState solventado

--- AnimalCooking/EndState.cpp
+++ AnimalCooking/EndState.cpp
@@ -27,7 +27,7 @@ EndState::EndState(AnimalCooking* ac) :State(ac), score(0), maxScore(SDLGame::in
 		nextLevelLimit = nivel["OneStar"].as_int();
 
 	//maxScore = 150;
-	//score = 150*.60;
+	//score = 150*.10;
 
 	createButtons(nextLevelLimit);
 	GPadController::instance()->sleepController(0, 3000);
@@ -63,7 +63,7 @@ void EndState::goToMapState(AnimalCooking* ac) {
 		fsm->popState();
 	}
 
-	MapConfig m;
+	MapConfig m(SDLGame::instance()->getName(),false);
 	m.save();
 }
 void EndState::goToMenuState(AnimalCooking* ac) {
