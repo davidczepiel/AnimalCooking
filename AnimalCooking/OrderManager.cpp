092581b0e7d6669ec594487c1f78129cfa20bdba
Alonefcp <fcuadra@ucm.es>
2020-09-21 13:46:08 +0200

    Nivelado nivel final

--- AnimalCooking/OrderManager.cpp
+++ AnimalCooking/OrderManager.cpp
@@ -172,6 +172,7 @@ double OrderManager::getTimePerOrder(Resources::FoodType finalProduct, int size)
 	case Resources::FoodType::RiceAndClams: time += timeCooking;
 	case Resources::FoodType::IcedRiceAndClams: time += timeCooking + iceDelay;
 	case Resources::FoodType::IcedRiceDish: time += timeCooking + iceDelay;
+	case Resources::FoodType::Teriyaki1: time += 2 * timeCooking+1000;
 	case Resources::FoodType::Teriyaki2: time += 2*timeCooking;
 	case Resources::FoodType::Teriyaki3: time += 3 * timeCooking + 25000;
 	case Resources::FoodType::Pizza: time += timeCooking;
@@ -182,8 +183,8 @@ double OrderManager::getTimePerOrder(Resources::FoodType finalProduct, int size)
 	case Resources::FoodType::Pizza5: time += timeCooking;
 	case Resources::FoodType::Perrito: time += timeCooking;
 	case Resources::FoodType::Perrito1: time += timeCooking+1000;
-	case Resources::FoodType::Perrito2: time += timeCooking;
-	case Resources::FoodType::Perrito3: time += 2*timeCooking;
+	case Resources::FoodType::Perrito2: time += timeCooking+1000;
+	case Resources::FoodType::Perrito3: time += 2*timeCooking+1000;
 
 	default:
 		break;
